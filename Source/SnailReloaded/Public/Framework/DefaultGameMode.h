bb7c1617b3ade5227b0c5c99c10143d89cac6db8
bence056 <vargabencecsaba2005@gmail.com>
2023-04-05 16:43:55 +0200

    Project setup

---
+++ Source/SnailReloaded/Public/Framework/DefaultGameMode.h
@@ -0,0 +1,17 @@
+// SnailReloaded - ThoughtOverflow 2023 - All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DefaultGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAILRELOADED_API ADefaultGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/SnailReloaded/SnailReloaded.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SnailReloaded.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SnailReloaded, "SnailReloaded" );
