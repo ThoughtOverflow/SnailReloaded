da249b6d55f6a55049453c0bcb7ca3095bb3b229
bence056 <vargabencecsaba2005@gmail.com>
2023-05-25 19:24:41 +0200

    Fixed player kill win

--- Source/SnailReloaded/Private/Framework/Combat/Standard/StandardCombatGameState.cpp
+++ Source/SnailReloaded/Private/Framework/Combat/Standard/StandardCombatGameState.cpp
@@ -591,10 +591,6 @@ void AStandardCombatGameState::CheckForAlivePlayers()
 					bAttackersDead = false;
 					break;
 				}
-			}else
-			{
-				bAttackersDead = false;
-				break;
 			}
 		}
 		for(auto& PlayerState : GetAllPlayersOfTeam(GetTeamBySide(EBombTeam::Defender)))
@@ -606,10 +602,6 @@ void AStandardCombatGameState::CheckForAlivePlayers()
 					bDefendersDead = false;
 					break;
 				}
-			}else
-			{
-				bDefendersDead = false;
-				break;
 			}
 		}
 		if(bAttackersDead || bDefendersDead)
