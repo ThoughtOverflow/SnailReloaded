d1f6b9345d4e1858866efbe87755d125aa58aa4f
bence056 <vargabencecsaba2005@gmail.com>
2023-06-10 16:42:15 +0200

    Added scan mine

--- Source/SnailReloaded/Private/Gameplay/Gadgets/ScanMine.cpp
+++ Source/SnailReloaded/Private/Gameplay/Gadgets/ScanMine.cpp
@@ -3,3 +3,52 @@
 
 #include "Gameplay/Gadgets/ScanMine.h"
 
+#include "Components/SphereComponent.h"
+#include "Gameplay/Player/DefaultPlayerCharacter.h"
+
+AScanMine::AScanMine()
+{
+	GadgetHealthComponent->GameObjectType.ObjectType = EGameObjectTypes::ScanMine;
+	DetectionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("DetectionSphere"));
+	RootBox = CreateDefaultSubobject<UBoxComponent>(TEXT("RootBox"));
+	SetRootComponent(RootBox);
+	GadgetMesh->SetupAttachment(GetRootComponent());
+	DetectionSphere->SetSphereRadius(200.f);
+	DetectionSphere->SetupAttachment(GetRootComponent());
+	GadgetMesh->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootBox->SetGenerateOverlapEvents(true);
+	RootBox->SetCollisionResponseToAllChannels(ECR_Block);
+	RootBox->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+	RootBox->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	// DetectionSphere->OnComponentBeginOverlap.AddDynamic(this, &AScanMine::PlayerEntered);
+	// DetectionSphere->OnComponentEndOverlap.AddDynamic(this, &AScanMine::PlayerExit);
+}
+
+void AScanMine::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	TArray<AActor*> Actors;
+	DetectionSphere->GetOverlappingActors(Actors, ADefaultPlayerCharacter::StaticClass());
+	for(auto& Actor : Actors)
+	{
+		//check for line of sight:
+		ADefaultPlayerCharacter* PlayerCharacter = Cast<ADefaultPlayerCharacter>(Actor);
+		FHitResult HitResult;
+		FVector TraceStartLoc = GetActorLocation();
+		FVector TraceEndLoc = PlayerCharacter->GetActorLocation();
+		FCollisionQueryParams Params;
+		Params.AddIgnoredActor(PlayerCharacter);
+		// if(!OwningPlayerState->GetOwningController()) return;
+		if(GetWorld() && GetWorld()->LineTraceSingleByChannel(HitResult, TraceEndLoc, TraceStartLoc, ECC_Visibility, Params))
+		{
+			if(HitResult.GetActor() == this && Cast<ACombatPlayerState>(PlayerCharacter->GetPlayerState())->GetTeam() != GetOwningTeam())
+			{
+				//We hit the player - it is in line of sight
+				UE_LOG(LogTemp, Warning, TEXT("Revealed: %s"), *PlayerCharacter->GetName());
+			}
+		}
+	}
+	
+}
+
--- Source/SnailReloaded/Public/Gameplay/Gadgets/ScanMine.h
+++ Source/SnailReloaded/Public/Gameplay/Gadgets/ScanMine.h
@@ -3,9 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
 #include "Gameplay/Gadgets/Gadget.h"
 #include "ScanMine.generated.h"
 
+class USphereComponent;
 /**
  * 
  */
@@ -13,5 +15,24 @@ UCLASS()
 class SNAILRELOADED_API AScanMine : public AGadget
 {
 	GENERATED_BODY()
+
+public:
+
+	AScanMine();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mine properties")
+	float BootupTime;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mine properties")
+	float DetectionTime;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mine properties")
+	USphereComponent* DetectionSphere;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Mine properties")
+	UBoxComponent* RootBox;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+protected:
+
+
 	
 };
