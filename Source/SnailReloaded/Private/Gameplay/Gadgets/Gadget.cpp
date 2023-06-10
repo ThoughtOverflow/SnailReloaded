2b7bd658a5f13aa1cb7ddd64013474d503d72163
bence056 <vargabencecsaba2005@gmail.com>
2023-06-10 15:52:57 +0200

    Added gadget repl

--- Source/SnailReloaded/Private/Gameplay/Gadgets/Gadget.cpp
+++ Source/SnailReloaded/Private/Gameplay/Gadgets/Gadget.cpp
@@ -12,6 +12,9 @@ AGadget::AGadget()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	bReplicates = true;
+	SetReplicateMovement(true);
+
 	GadgetHealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("GadgetHeath"));
 	GadgetHealthComponent->DefaultObjectHealth = 100.f;
 	GadgetHealthComponent->SetObjectMaxHealth(100.f);
