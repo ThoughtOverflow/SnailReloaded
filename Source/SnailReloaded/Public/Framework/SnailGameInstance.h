da4a45f8a8e412ff720b708a588765d392785e5d
bence056 <vargabencecsaba2005@gmail.com>
2023-06-13 20:46:03 +0200

    eos start

--- Source/SnailReloaded/Public/Framework/SnailGameInstance.h
+++ Source/SnailReloaded/Public/Framework/SnailGameInstance.h
@@ -9,6 +9,11 @@
 #define GOLDEN_SENS (0.130975f);
 
 class USettingsSavegame;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnEpicLoginComplete);
+
+class FOnlineSessionSearch;
+DECLARE_LOG_CATEGORY_EXTERN(LogOnlineGameSession, Log, All);
+
 /**
  * 
  */
@@ -41,5 +46,47 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	void SaveSettingsSavegameToDisk();
+
+	//EOS related - yoinked from CouncilGame
+
+	/**
+ * @addtogroup ds_settings
+ * @brief The name of the game server
+ */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Session Settings")
+	FString GameServerName;
+	/**
+	 * @addtogroup ds_settings
+	 * @brief The admin password for the game specified.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Session Settings")
+	FString AdminGamePassword;
+	/**
+	 * @addtogroup ds_settings
+	 * @brief Max player count
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Session Settings")
+	int32 maxPlayerCount;
+	/**
+	 * @addtogroup ds_settings
+	 * @brief Whether to use developer authentication or not.
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Game Settings")
+	bool bUseDevAuth;
+	/**
+	 * @addtogroup ds_settings
+	 * @brief Reference to the savegame storing the user's config.
+	 */
+	// UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Savegames")
+	// UPlayerConfigSavegame* PlayerConfigSavegame;
+
+
+	//delegates:
+
+	/**
+	 * @brief Fired when the login through epic games is finished.
+	 */
+	UPROPERTY()
+	FOnEpicLoginComplete EpicLoginComplete;
 	
 };
